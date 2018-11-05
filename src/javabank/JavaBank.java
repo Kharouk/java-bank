6615482352137caed62337f71da5179251a9cb1a
Alex Kharouk <akharoukpp@gmail.com>
2018-11-05 13:08:43 +0000

    get user input and test to make sure they are greeted fairly

--- src/javabank/JavaBank.java
+++ src/javabank/JavaBank.java
@@ -1,7 +1,23 @@
 package javabank;
 
+import java.util.Scanner;
+
+
+
 public class JavaBank {
+    public static void main(String[] args) {
+
+    }
+
     public String greeting() {
         return "JavaBank is now Open!";
     }
+
+    public String sayingHello() {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("What is your name?");
+        String name = reader.nextLine();
+        return String.format("Nice to see you %s, welcome to JavaBank!", name);
+    }
+
 }
--- src/javabank/JavaBankTest.java
+++ src/javabank/JavaBankTest.java
@@ -1,13 +1,38 @@
 package javabank;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 
 class JavaBankTest {
 
+    @BeforeEach
+    void initBank() throws Exception {
+        System.out.println("Hello");
+    }
+
     @Test
     void checksIfWorking() {
-        JavaBank javabank = new JavaBank();
-        assertEquals("JavaBank is now Open!", javabank.greeting());
+        JavaBank barclays = new JavaBank();
+        assertEquals("JavaBank is now Open!", barclays.greeting());
+    }
+
+    @Test
+    void inputMessage() {
+        JavaBank barclays = new JavaBank();
+        String input = "Alex";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        assertEquals("Nice to see you Alex, welcome to JavaBank!", barclays.sayingHello());
+    }
+
+    @Test
+    void checkBalanceOnAccount() {
+
     }
 }
