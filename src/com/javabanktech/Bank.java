1f37f812467c9ca4ab4a9630a3cf09e45837508e
Alex Kharouk <akharoukpp@gmail.com>
2018-11-06 09:28:11 +0000

    refactor Bank to work in console and add Main class to control app

--- src/com/javabanktech/Bank.java
+++ src/com/javabanktech/Bank.java
@@ -1,8 +1,8 @@
 package com.javabanktech;
 
-import java.text.DecimalFormat;
-import java.text.NumberFormat;
-import java.util.Scanner;
+        import java.text.DecimalFormat;
+        import java.text.NumberFormat;
+        import java.util.Scanner;
 
 public class Bank {
     private double balance;
@@ -12,28 +12,29 @@ public class Bank {
 
     }
 
-    public String greeting() {
-        return "JavaBank is now Open!";
+    public void greeting() {
+        System.out.println("JavaBank is now Open!");
     }
 
-    public String sayingHello() {
+    public void sayingHello() {
         Scanner reader = new Scanner(System.in);
         System.out.println("Are you changing your name?");
         String name = reader.nextLine();
         if (name.equals("no") || name.equals("n")) {
-            return "Fair enough. Have a wonderful day!";
+            System.out.println("Fair enough. Have a wonderful day!");
         } else {
             this.accountName = name;
-            return String.format("That's a great new name %s, enjoy your time at the JavaBank!", accountName);
+            System.out.println(String.format("That's a great new name %s, enjoy your time at the JavaBank!", accountName));
         }
     }
 
-    public String newAccount(Person person) {
+    public void newAccount(Person person) {
         double d = person.getBalance();
         NumberFormat formatter = new DecimalFormat("#.00");
         this.balance = d;
         this.accountName = person.getName();
-        return String.format("Thanks for signing up with JavaBank, %s. Your current total is $"+ formatter.format(d), accountName);
+        System.out.println(String.format("Thanks for signing up with JavaBank, %s. Your current total is $"+ formatter.format(d), accountName));
     }
 
 }
+
--- src/com/javabanktech/Main.java
+++ src/com/javabanktech/Main.java
@@ -1,7 +1,11 @@
 package com.javabanktech;
 
-public class Main {
+class Main {
     public static void main(String[] args) {
-        
+        Person person = new Person("Alex", 23, 100.00);
+        Bank tsb = new Bank();
+        tsb.greeting();
+        tsb.sayingHello();
+        tsb.newAccount(person);
     }
 }
