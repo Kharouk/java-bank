6ba1ea165a8062b5addfec2f11d0918f586c0b85
Alex Kharouk <akharoukpp@gmail.com>
2018-12-03 16:02:04 +0000

    Gets User's input to change the default language setting.

--- src/com/javabanktech/LanguageSupport.java
+++ src/com/javabanktech/LanguageSupport.java
@@ -1,5 +1,7 @@
 package com.javabanktech;
 
+import java.util.Scanner;
+
 public class LanguageSupport {
     private String language = "English";
 
@@ -8,7 +10,12 @@ public class LanguageSupport {
     }
 
     void beginLaunch() {
+        Scanner input = new Scanner(System.in);
         System.out.println(String.format("We are sorry that we have defaulted to %s. What language do you prefer?", this.language));
+        String preferredLanguage = input.nextLine();
+        this.language = preferredLanguage;
+        System.out.println(String.format("Okay, your preference of %s as being the default language has been saved.\n", this.language));
+        System.out.println("Please allow 10-15 Business Days for the changes to be made. Thank you for using JavaBank!");
     }
 
     public String getLanguage() {
