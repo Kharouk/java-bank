9de62717314f2fa1ba06594bed0ec19f60d1012c
Alex Kharouk <akharoukpp@gmail.com>
2018-12-03 15:55:57 +0000

    Test for default langauge and getter method

--- src/com/javabanktech/LanguageSupport.java
+++ src/com/javabanktech/LanguageSupport.java
@@ -2,11 +2,16 @@ package com.javabanktech;
 
 public class LanguageSupport {
     private String language = "English";
+
     public void main(String[] args) {
         beginLaunch();
     }
 
     void beginLaunch() {
         System.out.println(String.format("We are sorry that we have defaulted to %s. What language do you prefer?", this.language));
+    }
+
+    public String getLanguage() {
+        return language;
     }
 }
--- tests/com/javabanktech/LanguageSupportTest.java
+++ tests/com/javabanktech/LanguageSupportTest.java
@@ -1,7 +1,16 @@
 package com.javabanktech;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class LanguageSupportTest {
+    LanguageSupport language = new LanguageSupport();
 
+    @Test
+    void TestingForDefaultLanguage() {
+        String expected = "English";
+        assertEquals(expected, language.getLanguage());
+    }
 }
